dcd950e8a4c09a16cc984f7a055890b2789697c2
minsunki <minsunki@student.42seoul.kr>
2021-04-09 01:06:20 +0900

    fix

--- ft_print_dec.c
+++ ft_print_dec.c
@@ -6,7 +6,7 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 18:02:34 by minsunki          #+#    #+#             */
-/*   Updated: 2021/04/09 00:45:40 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/04/09 01:06:03 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,15 +116,28 @@ int			ft_print_dec(long long num, t_cvd *cvd)
 			wid -= ft_putc('-');
 		if (cvd->flag & e_prec)
 			wid -= ppad('0', cvd->pwidth - g_nw);
+		if (!num && !cvd->pwidth)
+		{
+			if (cvd->width)
+				wid -= ppad(' ', wid);
+			return (cvd->width);
+		}
 		wid -= putnbr(num < 0 ? -num : num);
 		wid -= ppad(' ', wid);
 	}
 	else
 	{
-		wid -= ppad(((cvd->flag & e_zfill) && !(cvd->flag & e_prec) ? '0' : ' '), wid - ft_max(cvd->pwidth, g_nw) - (num < 0));
+		if (cvd->pwidth >= 0)
+			wid -= ppad(((cvd->flag & e_zfill) && !(cvd->flag & e_prec) && (num > 0) ? '0' : ' '), wid - ft_max(cvd->pwidth, g_nw) - (num < 0));
 		if (num < 0)
 			wid -= ft_putc('-');
-		wid -= ppad('0', cvd->pwidth - g_nw);
+		if (num == 0 && !cvd->pwidth)
+		{
+			if (cvd->width)
+				wid -= ppad(' ', wid);
+			return (cvd->width);
+		}
+		wid -= ppad('0', wid - g_nw);
 		wid -= putnbr(num < 0 ? -num : num);
 	}
 	return (ft_max(ft_max(cvd->pwidth + (num < 0), cvd->width), g_nw + (num < 0)));
