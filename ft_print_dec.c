3951b9fb0c493f2b03917ef23d29fb61cea2a4fc
minsunki <minsunki@student.42seoul.kr>
2021-04-09 01:14:49 +0900

    fix

--- ft_print_dec.c
+++ ft_print_dec.c
@@ -6,7 +6,7 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 18:02:34 by minsunki          #+#    #+#             */
-/*   Updated: 2021/04/09 01:06:03 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/04/09 01:14:07 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,7 +127,7 @@ int			ft_print_dec(long long num, t_cvd *cvd)
 	}
 	else
 	{
-		if (cvd->pwidth >= 0)
+		if (cvd->pwidth)
 			wid -= ppad(((cvd->flag & e_zfill) && !(cvd->flag & e_prec) && (num > 0) ? '0' : ' '), wid - ft_max(cvd->pwidth, g_nw) - (num < 0));
 		if (num < 0)
 			wid -= ft_putc('-');
@@ -137,7 +137,8 @@ int			ft_print_dec(long long num, t_cvd *cvd)
 				wid -= ppad(' ', wid);
 			return (cvd->width);
 		}
-		wid -= ppad('0', wid - g_nw);
+		if (cvd->flag & e_zfill)
+			wid -= ppad('0', wid - g_nw);
 		wid -= putnbr(num < 0 ? -num : num);
 	}
 	return (ft_max(ft_max(cvd->pwidth + (num < 0), cvd->width), g_nw + (num < 0)));
