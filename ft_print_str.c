e3d7c51fdaba2d530d6b0ccd3bae5a3a00f39a0f
minsunki <minsunki@student.42seoul.kr>
2021-04-12 21:33:43 +0900

    ft_print_str fix

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,23 +6,31 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 17:37:54 by minsunki          #+#    #+#             */
-/*   Updated: 2021/04/12 21:01:53 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/04/12 21:31:47 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int			ft_print_str(const char *str, t_cvd *cvd)
 {
 	int		slen;
 	int		wid;
 	int		owid;
+	int		nullp;
 
+	nullp = 0;
 	if (!str)
-		str = (cvd->pwidth >= 6 ? "(null)" : "");
+	{
+		nullp = 1;
+		str = "(null)";
+	}
 	slen = ft_strlen(str);
 	if ((cvd->flag & e_prec) && cvd->pwidth >= 0)
 		slen = ft_min(cvd->pwidth, slen);
+	if (nullp && slen < 6)
+		str = "     ";
 	wid = ft_max(cvd->width, slen);
 	owid = wid;
 	if (!(cvd->flag & e_lalign))
