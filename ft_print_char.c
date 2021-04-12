47f4182de882df0f9132a773d7d5bbc7cff31aba
minsunki <minsunki@student.42seoul.kr>
2021-04-12 19:12:27 +0900

    fix ft_print_char

--- ft_print_char.c
+++ ft_print_char.c
@@ -6,11 +6,12 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 00:20:20 by minsunki          #+#    #+#             */
-/*   Updated: 2021/04/03 17:37:37 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/04/12 19:11:33 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int			ft_print_char(int c, t_cvd *cvd)
 {
@@ -19,8 +20,8 @@ int			ft_print_char(int c, t_cvd *cvd)
 	ret = cvd->width;
 	if (cvd->flag & e_lalign)
 		ft_putchar_fd(c, 1);
-	while (--cvd->width)
-		ft_putchar_fd((cvd->flag & e_zfill ? '0' : ' '), 1);
+	while (--cvd->width > 0)
+		ft_putchar_fd(' ', 1);
 	if (!(cvd->flag & e_lalign))
 		ft_putchar_fd(c, 1);
 	return (ret ? ret : 1);
