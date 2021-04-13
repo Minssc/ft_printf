7347300c553a027f1b903482d0689abd674c9d33
minsunki <minsunki@student.42seoul.kr>
2021-04-13 15:49:49 +0900

    fix ft_print_udec.c

--- ft_print_udec.c
+++ ft_print_udec.c
@@ -6,15 +6,16 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/05 00:22:29 by minsunki          #+#    #+#             */
-/*   Updated: 2021/04/05 16:11:11 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/04/13 15:47:39 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	get_width(unsigned int num, int base)
+static int	get_width(unsigned int num, int hex)
 {
-	int		ret;
+	const int	base = (hex ? 16 : 10);
+	int			ret;
 
 	ret = 0;
 	while (num)
@@ -22,51 +23,48 @@ static int	get_width(unsigned int num, int base)
 		num /= base;
 		ret++;
 	}
-	return (ret);
+	return (ret ? ret : 1);
 }
 
-#include <stdio.h>
-static int	print_unbr(unsigned int num, t_cvd *cvd)
+static int	putunbr(unsigned int num, int hex)
 {
-	int				ret;
-	unsigned int	base;
+	const int	base = (hex ? 16 : 10);
+	int			ret;
 
 	ret = 0;
-	base = (cvd->type == 'u' ? 10 : 16);
-	while (cvd->pwidth-- > 0)
-		ft_putchar_fd('0', 1);
 	if (num >= base)
-		print_unbr(num / base, cvd);
-	if (cvd->type == 'X')
-		ft_putchar_fd("0123456789ABCDEF"[num % base], 1);
+		ret += putunbr(num / base, hex);
+	if (hex == 2)
+		ft_putc("0123456789ABCDEF"[num % base]);
 	else
-		ft_putchar_fd("0123456789abcdef"[num % base], 1);
-	return (ret);
+		ft_putc("0123456789abcdef"[num % base]);
+	return (1 + ret);
 }
 
 int			ft_print_udec(unsigned int num, t_cvd *cvd)
 {
-	int		ret;
-	int		nw;
-	int		pw;
-	int		base;
+	const int	hex = !(cvd->type == 'u') + (cvd->type == 'X');
+	const int	nw = get_width(num, hex);
+	int			wid;
+	int			owid;
+	int			pw;
 
-	base = (cvd->type == 'u' ? 10 : 16);
-	nw = get_width(num, base);
-	pw = (nw < cvd->pwidth ? cvd->pwidth : nw);
-	ret = pw - nw;
-	if (cvd->flag & e_lalign)
-		ret += print_unbr(num, cvd);
-	if (pw < cvd->width)
-	{
-		ret += cvd->width - pw;
-		while (cvd->width-- - pw)
-			if (cvd->flag & e_lalign || cvd->flag & e_prec)
-				ft_putchar_fd(' ', 1);
-			else
-				ft_putchar_fd((cvd->flag & e_zfill ? '0' : ' '), 1);
-	}
+	wid = ft_max(ft_max(cvd->pwidth, cvd->width), nw);
+	owid = wid;
+	pw = nw;
+	if (!(cvd->flag & e_lalign) && (cvd->flag & e_zfill))
+		pw = wid;
+	if ((cvd->flag & e_prec) && cvd->pwidth >= 0)
+		pw = ft_max(cvd->pwidth, nw);
 	if (!(cvd->flag & e_lalign))
-		ret += print_unbr(num, cvd);
-	return (ret + nw);
+		wid -= ft_pad(' ', wid - pw);
+	wid -= ft_pad('0', pw - nw);
+	if (!num && ((cvd->flag & e_prec) && !cvd->pwidth))
+	{
+		wid -= (cvd->width ? ft_pad(' ', wid) : 0);
+		return (cvd->width);
+	}
+	wid -= putunbr(num, hex);
+	wid -= ft_pad(' ', wid);
+	return (owid);
 }
