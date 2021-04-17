d37a57aebc4c48131bb976075e53a32e5ce8dc2c
minssc (minsunki) <pauaup@naver.com>
2021-04-17 12:40:18 +0900

    fix pft test 130 fail

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: minsunki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/03 17:37:54 by minsunki          #+#    #+#             */
-/*   Updated: 2021/04/12 21:49:19 by minsunki         ###   ########.fr       */
+/*   Updated: 2021/04/17 12:39:29 by minsunki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int			ft_print_str(const char *str, t_cvd *cvd)
 		slen = ft_min(cvd->pwidth, slen);
 	wid = ft_max(cvd->width, slen);
 	if (!(cvd->flag & e_lalign))
-		wid -= ft_pad(' ', wid - slen);
+		wid -= ft_pad((cvd->flag & e_zfill ? '0' : ' '), wid - slen);
 	wid -= ft_nputs(str, slen);
 	wid -= ft_pad(' ', wid);
 	return (ft_max(cvd->width, slen));
